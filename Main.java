26a30aecc9ce5186119cfd37fe6776ca505170b9
karajus <karaszkiewiczjustyna@gmail.com>
2024-10-26 15:29:16 +0200

    26.10

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
+import java.sql.Array;
 import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -59,17 +62,68 @@ public class Main {
 
 //
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("To jest program mówiacy czy liczba jest parzysta lub nieparzyta /n Wpisz liczbę: ");
-        int inputNumber = input.nextInt();
-        input.close();
-        if (inputNumber % 2 == 0) {
-            System.out.println("Liczba jest parzysta.");
-        } else {
-            System.out.println("Liczba jest nie parzysta.");
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("To jest program mówiacy czy liczba jest parzysta lub nieparzyta /n Wpisz liczbę: ");
+//        int inputNumber = input.nextInt();
+//        input.close();
+//        if (inputNumber % 2 == 0) {
+//            System.out.println("Liczba jest parzysta.");
+//        } else {
+//            System.out.println("Liczba jest nie parzysta.");
+//
+//        }
 
+
+//        //STAŁA WIEKĄ LITERĄ
+//        final  int CONDITION_NUMBER= 10;
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("To jest program mówiacy czy liczba jest większa od 10.\nWpisz liczbę: ");
+//        int inputNumber = input.nextInt();
+//        input.close();
+//        if (inputNumber > CONDITION_NUMBER) {
+//            System.out.println("Liczba jest większa od" + CONDITION_NUMBER + ".");
+//        } else if (inputNumber == CONDITION_NUMBER)
+//            System.out.println("Liczba jest równa 10" + CONDITION_NUMBER + ".");
+//
+//        else {
+//            System.out.println("Liczba jest mniejsza od 10.");
+//
+//        }
+        //Arrays.stream(args).forEach(System..)
+
+
+        boolean count = false;
+        while (!count){
+            //STAŁA WIEKĄ LITERĄ
+            //final  int CONDITION_NUMBER= 10;
+            Scanner input = new Scanner(System.in);
+            System.out.println("Wpisz liczbę: ");
+
+            try {
+                int inputNumber = input.nextInt();
+                input.close();
+                if (inputNumber < 1){
+                    throw new InputMismatchException();
+
+                }else {
+
+                    for (int j = 1; j <= inputNumber; j++) {
+                        for (int i = 1; i <= inputNumber; i++) {
+                            if (i == inputNumber) {
+                                System.out.print(i + "\n");
+                            } else {
+                                System.out.print(i + ", ");
+                            }
+                        }
+                    }
+                    count = true;
+                }
+            } catch (InputMismatchException e){
+                System.out.println("Proszę podac liczbę całkowitą");
+
+
+            }
         }
-
 
 
 
