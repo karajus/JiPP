6cb45b451e89deb0625fa4bbd1ce52cdeb5bf0ec
Kaliszek47 <nkaliszek@gmail.com>
2024-11-09 14:28:39 +0100

    09.11

--- Exercise.java
+++ Exercise.java
@@ -96,63 +96,122 @@ public class Exercise {
 //        System.out.println("Całkowity Koszt kredytu wyniesie: " + calKoszt);
 //        System.out.println("rata miesieczna będzie wynosiła: " + ratamies);
 
-
-        char operator;
-        double number1, number2, result;
-
-        Scanner input = new Scanner(System.in);
-        while(true) {
-            System.out.println("Choose an operator: +, -, *, or /");
-            operator = input.next().charAt(0);
-            if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
-                break;
-            } else {
-                System.out.println("Invalid operator, try again!");
-            }
-        }
-
-        // ask users to enter numbers
-        System.out.println("Enter first number: ");
-        number1 = input.nextDouble();
-
-        System.out.println("Enter second number: ");
-        number2 = input.nextDouble();
-
-        switch (operator) {
-
-            // performs addition between numbers
-            case '+':
-                result = number1 + number2;
-                System.out.println(number1 + " + " + number2 + " = " + result);
-                break;
-
-            // performs subtraction between numbers
-            case '-':
-                result = number1 - number2;
-                System.out.println(number1 + " - " + number2 + " = " + result);
-                break;
-
-            // performs multiplication between numbers
-            case '*':
-                result = number1 * number2;
-                System.out.println(number1 + " * " + number2 + " = " + result);
-                break;
-
-            // performs division between numbers
-            case '/':
-                if (number2 == 0) {
-                    System.out.println("Zero devision error");
-                }else {
-                    result = number1 / number2;
-                    System.out.println(number1 + " / " + number2 + " = " + result);
-                }
-                break;
-
-//            default:
-//                System.out.println("Invalid operation!");
+// KALKULATOR
+//        char operator;
+//        double number1, number2, result;
+//
+//        Scanner input = new Scanner(System.in);
+//        while(true) {
+//            System.out.println("Choose an operator: +, -, *, or /");
+//            operator = input.next().charAt(0);
+//            if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
 //                break;
+//            } else {
+//                System.out.println("Invalid operator, try again!");
+//            }
+//        }
+//
+//        // ask users to enter numbers
+//        System.out.println("Enter first number: ");
+//        number1 = input.nextDouble();
+//
+//        System.out.println("Enter second number: ");
+//        number2 = input.nextDouble();
+//
+//        switch (operator) {
+//
+//            // performs addition between numbers
+//            case '+':
+//                result = number1 + number2;
+//                System.out.println(number1 + " + " + number2 + " = " + result);
+//                break;
+//
+//            // performs subtraction between numbers
+//            case '-':
+//                result = number1 - number2;
+//                System.out.println(number1 + " - " + number2 + " = " + result);
+//                break;
+//
+//            // performs multiplication between numbers
+//            case '*':
+//                result = number1 * number2;
+//                System.out.println(number1 + " * " + number2 + " = " + result);
+//                break;
+//
+//            // performs division between numbers
+//            case '/':
+//                if (number2 == 0) {
+//                    System.out.println("Zero devision error");
+//                }else {
+//                    result = number1 / number2;
+//                    System.out.println(number1 + " / " + number2 + " = " + result);
+//                }
+//                break;
+//
+////            default:
+////                System.out.println("Invalid operation!");
+////                break;
+//        }
+//
+//        input.close();
+
+        //wczytać 2 liczby A < B, wyznaczyć sumę ciągu liczb A, A+1 ... do B. oblicz 3 razy używając pętli while, do-while, for
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Podaj 2 cyfry całkowite:  ");
+//        int A = input.nextInt();
+//        int B = input.nextInt();
+//        input.close();
+//
+//        int sum = A;
+////        if (A < B) {
+////
+////            for (int i = A; i < B+1; i++) {
+////                sum += i;
+////            }
+////        } else {
+////            System.out.println("A musi być mniejsze od B!");
+////        }
+////        System.out.println(sum);
+////        while(A < B) {
+////            sum += A + 1;
+////            A++;
+////        }
+////        System.out.println(sum);
+//        do {
+//            sum += A + 1;  // Add A + 1 to sum
+//            A++;           // Increment A
+//        } while (A < B);   // Continue while A is still less than B
+//
+//        System.out.println(sum);
+
+
+        //obliczamy podatek
+//        - do 85.528 podatek wynosi 18% podstawy minus 556,02 pln
+        // - od 85.528 podatek wyunosi 14,839,02 + 32 % nadwyżki
+        Scanner input = new Scanner(System.in);
+
+        // Pobieramy dochód użytkownika
+        System.out.print("Podaj dochód: ");
+        double dochod = input.nextDouble();
+
+        // Zmienna do przechowywania wartości podatku
+        double podatek = 0;
+
+        // Obliczanie podatku na podstawie podanych stawek
+        if (dochod <= 85_528) {
+            // Stawka 18% do 85 528 PLN
+            podatek = dochod * 0.18 - 556.02;
+        } else {
+            // Stawka 32% powyżej 85 528 PLN
+            podatek = 14_389.02 + (dochod - 85_528) * 0.32;
         }
 
+        // Wyświetlenie wyniku
+        System.out.printf("Podatek do zapłaty: %.2f PLN\n", podatek);
+        //(tax < 0 ? 0 : tax)
+
+        // Zamykamy obiekt Scanner
         input.close();
     }
 }
